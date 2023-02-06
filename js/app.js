be32624f9464d8e2450395e1f6fb836b2b4903eb
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-06 15:08:30 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+// In questo esercizio è importante seguire le milestone per affrontare in maniera via via crescente le difficoltà di ogni problema.
+
+
+
