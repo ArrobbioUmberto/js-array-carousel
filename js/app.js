3a6c14f207de2e274bedb77d037a63c79faf1621
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-06 16:51:21 +0100

    FEAT js for carousel scroll

--- js/app.js
+++ js/app.js
@@ -3,3 +3,51 @@
 
 
 
+let picture = ['./img/img1.jpeg', './img/img2.jpeg', './img/img3.jpeg', './img/img4.webp', './img/img5.jpg', './img/img6.webp']
+
+const slideElements = document.querySelectorAll('.slide')
+console.log(slideElements)
+const arrowElementRight = document.querySelector('.arrow-right')
+console.log(arrowElementRight)
+const arrowElementLeft = document.querySelector('.arrow-left')
+console.log(arrowElementLeft)
+
+let indiceSlideAttiva = 0
+
+// milestone 2 Al click dell’utente sulle frecce (una a sinistra e una a destra), il programma cambierà l’immagine attiva, 
+// che quindi verrà visualizzata al posto della precedente. Attenzione quando arriviamo all’ultima slide e 
+// clicchiamo sulla freccia destra, o viceversa se siamo sulla prima e clicchiamo sulla freccia sinistra.
+
+arrowElementRight.addEventListener('click', function () {
+    console.log('current slide', indiceSlideAttiva)
+
+	let slideCorrente = slideElements[indiceSlideAttiva]
+	// togliendo la classe active
+	slideCorrente.classList.remove('active')
+
+	// inremento l'indice
+	indiceSlideAttiva += 1
+
+	let prossimaSlide = slideElements[indiceSlideAttiva]
+	// aggiungiamo la classe active alla seconda slide
+	prossimaSlide.classList.add('active')
+
+	console.log('next slide', indiceSlideAttiva)
+})
+arrowElementLeft.addEventListener('click', function(){
+    console.log('current slide', indiceSlideAttiva)
+
+	let slideCorrente = slideElements[indiceSlideAttiva]
+	// togliendo la classe active
+	slideCorrente.classList.remove('active')
+
+	// inremento l'indice
+	indiceSlideAttiva -= 1
+
+	let prossimaSlide = slideElements[indiceSlideAttiva]
+	// aggiungiamo la classe active alla seconda slide
+	prossimaSlide.classList.add('active')
+
+	console.log('next slide', indiceSlideAttiva)
+})
+
